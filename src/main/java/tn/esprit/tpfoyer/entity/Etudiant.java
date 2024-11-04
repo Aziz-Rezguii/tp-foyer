05a7c5cd2f0a55c63c00c645dde53b69f27e11db
Aziz-Rezguii <rezgui01aziz@gmail.com>
2024-11-04 21:44:52 +0100

    pom modification

--- src/main/java/tn/esprit/tpfoyer/entity/Etudiant.java
+++ src/main/java/tn/esprit/tpfoyer/entity/Etudiant.java
@@ -1,17 +1,8 @@
 package tn.esprit.tpfoyer.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
-import lombok.AccessLevel;
 
 import java.util.Date;
 import java.util.Set;
@@ -20,6 +11,8 @@ import java.util.Set;
 @Getter
 @Setter
 @AllArgsConstructor
+
+
 @NoArgsConstructor
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -37,11 +30,11 @@ public class Etudiant {
     @ManyToMany(mappedBy = "etudiants")
     Set<Reservation> reservations;
 
-    // Constructeur personnalisé avec les principaux attributs
     public Etudiant(String nomEtudiant, String prenomEtudiant, long cinEtudiant, Date dateNaissance) {
         this.nomEtudiant = nomEtudiant;
         this.prenomEtudiant = prenomEtudiant;
         this.cinEtudiant = cinEtudiant;
         this.dateNaissance = dateNaissance;
     }
+
 }
